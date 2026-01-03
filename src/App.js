21fd9f7eff06e16ad4dfe782221e8f05d6e08468
nadr-husin <147298735+nadr-husin@users.noreply.github.com>
2026-01-03 14:26:29 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,91 @@
+import background from './assets/background-img.jpg';
+import { motion } from "framer-motion";
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTiktok } from "@fortawesome/free-brands-svg-icons";
+
+function App() {
+  return (
+    <div
+      className="container"
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "100vh",
+      }}
+    >
+      <motion.div className="parent">
+        <h2>MASA</h2>
+
+        <motion.a
+          href="https://www.facebook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          initial={{ opacity: 0, y: -250 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.7 }}
+          className="btn"
+        >
+          <h4><FacebookIcon style={{ fontSize: "35px" }} /> Latest Updates</h4>
+        </motion.a>
+
+        <motion.a
+          href="https://www.instagram.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          initial={{ opacity: 0, x: -250 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.7 }}
+          className="btn"
+        >
+          <h4><InstagramIcon style={{ fontSize: "35px" }} /> Style Inspiration</h4>
+        </motion.a>
+
+        <motion.a
+          href="https://www.tiktok.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          initial={{ opacity: 0, x: 250 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.7 }}
+          className="btn"
+        >
+          <h4>
+            <FontAwesomeIcon icon={faTiktok} style={{ fontSize: "35px" }} />
+            Fashion Videos
+          </h4>
+        </motion.a>
+
+        <motion.a
+          href="https://wa.me/201234567890"
+          target="_blank"
+          rel="noopener noreferrer"
+          initial={{ opacity: 0, y: 250 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.7 }}
+          className="btn"
+        >
+          <h4><WhatsAppIcon style={{ fontSize: "35px" }} /> Contact With Us</h4>
+        </motion.a>
+
+
+
+
+        <h3>
+          Made by <motion.a  href="https://www.instagram.com/nadrhusein/"
+          target="_blank"
+          rel="noopener noreferrer"
+           style={{ color: "#722F37" , textDecoration: "none"}}>Nader</motion.a>
+        </h3>
+      </motion.div>
+
+      <div className="sparkle-overlay"></div>
+      <div className="simple-overlay"></div>
+    </div>
+  );
+}
+
+export default App;
